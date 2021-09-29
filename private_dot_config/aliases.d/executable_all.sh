2c8e753509dc8e287ea6cc9835a2d19bcdeb7194
Yannic Nöhring <yannicnoeh@gmail.com>
2021-09-29 17:57:45 +0200

    Update dot_bash_profile Update private_dot_config/aliases.d/executable_all.sh

--- private_dot_config/aliases.d/executable_all.sh
+++ private_dot_config/aliases.d/executable_all.sh
@@ -1,1 +1,13 @@
 #! /bin/sh
+#sudo
+alias sudo='sudo '
+#ls
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+#cd
+alias ..='cd ..'
+alias ~='cd ~'
+#aptitude
+command -v aptitude >/dev/null 2>&1 && alias apt='aptitude'
+#tar
+command -v tar >/dev/null 2>&1 && alias untar='tar -zxvf '
